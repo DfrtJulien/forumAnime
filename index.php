3a65e2bc13191cef31fc7d20b26b0281e86e9470
DfrtJulien <de.freitas.julien78@gmail.com>
2024-09-18 11:17:58 +0200

    création du router et de la vue homme et controller

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
-    include_once (__DIR__ . '/app/Views/partials/head.php');
+    require_once (__DIR__ . '/config/router.php');
 ?>
 
 
-<?php
-    include_once (__DIR__ . '/app/Views/partials/footer.php');
-?>
