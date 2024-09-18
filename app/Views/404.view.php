88fbf2ac67cb5644ff39b12d99a87da1ff9116d2
DfrtJulien <de.freitas.julien78@gmail.com>
2024-09-18 12:11:16 +0200

    mise en place de la page 404 avec la redirection

---
+++ app/Views/404.view.php
@@ -0,0 +1,14 @@
+<?php
+    require_once (__DIR__ . '/partials/head.php');
+?>
+
+    <div class="errorContainer">
+        <h1>Erreur 404</h1>
+        <img src="/public//img//erreur404.png" alt="erreur 404 plus fort que luffy" class="img-error">
+        <button class="errorBtn" href="/"><a href="/">Revenir a l'accueil</a></button>
+    </div>
+   
+
+<?php
+    include_once (__DIR__ . '/partials/footer.php');
+?>
--- app/Views/partials/footer.php
+++ app/Views/partials/footer.php
@@ -1,3 +1,3 @@
-<footer class="container-fluid bg-color-1"><?php echo date('Y') ?> Julien De Freitas</footer>
+<footer class="container-fluid bg-color-1 text-white fs-3"><?php echo date('Y') ?> © Julien De Freitas</footer>
 </body>
 </html>
