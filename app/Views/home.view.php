8b7f33aa23e85c5b7ee4602f15822b16b63d86fd
DfrtJulien <de.freitas.julien78@gmail.com>
2024-09-20 13:39:19 +0200

    creation de l'affichage des sujets

--- app/Views/home.view.php
+++ app/Views/home.view.php
@@ -1,14 +1,30 @@
 <?php
     require_once(__DIR__ . "/partials/head.php");
+?>  
+    <div class="homeImg">
+        <img src="/public//img/hero-manga.png" alt="">
+  
+<?php
     if(isset($_SESSION['user'])){
         ?>
             <h1>Bienvenue <?= $_SESSION['user']['pseudo'] ?></h1>
-        <?php
+            <?php
+    if($subjects){
+
+    
+    foreach($subjects as $subject){
+        ?>
+        <h3><?= $subject['title'] ?></h3>
+        <p><?= $subject['description'] ?></p>
+    <?php
+    }
+}
     }else {
 ?>
     <h1>Bienvenue à toi !</h1>
-    
+      
 <?php
 }
     include_once(__DIR__ . "/partials/footer.php");
 ?>
+  </div>
